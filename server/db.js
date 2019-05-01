44c8728d265d3bb71a77c7b5943988799d37b3c6
Natalie Klein <NatalieKleinHome@Natalies-MacBook-Pro.local>
2019-04-30 19:10:25 -0700

    copied over data from keiths db, updated dbjs file with our new credentials, connected to node

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  user: 'srbxdloa',
+  user: 'phwujlax',
   host: 'isilo.db.elephantsql.com',
-  database: 'srbxdloa',
-  password: '3qX2xRdx-yM_30nR5Svg_kJcr_-I4m0A', // need to hide the password
+  database: 'phwujlax',
+  password: 'iJxOqxlqCBWkx7Bb73_6K2cMiYXEgOEG', // need to hide the password
   port: '5432',
 })
 
